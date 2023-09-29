8f19b9cf69e9ab75eeb8b33d19f856183e5cc5ba
Guillaume <guilbill@gmail.com>
2023-09-29 16:00:51 +0200

    drop unused tables

--- nhost/migrations/default/1695994121748_create_table_public_team/up.sql
+++ nhost/migrations/default/1695994121748_create_table_public_team/up.sql
@@ -1,1 +1,10 @@
-CREATE TABLE public.team (id uuid DEFAULT gen_random_uuid() NOT NULL, "playerOne" uuid NOT NULL, "playerTwo" uuid NOT NULL, PRIMARY KEY (id), FOREIGN KEY ("playerOne") REFERENCES public.player (id) ON UPDATE RESTRICT ON DELETE RESTRICT, FOREIGN KEY ("playerTwo") REFERENCES public.player (id) ON UPDATE RESTRICT ON DELETE RESTRICT);
+DROP TABLE IF EXISTS public.team CASCADE;
+
+CREATE TABLE public.team (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    "playerOne" uuid NOT NULL,
+    "playerTwo" uuid NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY ("playerOne") REFERENCES public.player (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+    FOREIGN KEY ("playerTwo") REFERENCES public.player (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
--- nhost/migrations/default/1695994309240_create_table_public_score/up.sql
+++ nhost/migrations/default/1695994309240_create_table_public_score/up.sql
@@ -1,1 +1,12 @@
-CREATE TABLE public.score (id uuid DEFAULT gen_random_uuid() NOT NULL, "blueTeamId" uuid NOT NULL, "redTeamId" uuid NOT NULL, "blueTeamScore" int4 NOT NULL, "redTeamScore" int4 NOT NULL, PRIMARY KEY (id), FOREIGN KEY ("blueTeamId") REFERENCES public.team (id) ON UPDATE RESTRICT ON DELETE RESTRICT, FOREIGN KEY ("redTeamId") REFERENCES public.team (id) ON UPDATE RESTRICT ON DELETE RESTRICT);
+DROP TABLE IF EXISTS public.score CASCADE;
+
+CREATE TABLE public.score (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    "blueTeamId" uuid NOT NULL,
+    "redTeamId" uuid NOT NULL,
+    "blueTeamScore" int4 NOT NULL,
+    "redTeamScore" int4 NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY ("blueTeamId") REFERENCES public.team (id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+    FOREIGN KEY ("redTeamId") REFERENCES public.team (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
