4da19c7f356271b1da5e346f0c98fc5bf9867c0e
Guillaume <guilbill@gmail.com>
2023-09-29 15:56:26 +0200

    drop unused tables

--- nhost/migrations/default/1695994069422_create_table_public_player/up.sql
+++ nhost/migrations/default/1695994069422_create_table_public_player/up.sql
@@ -1,1 +1,7 @@
-CREATE TABLE public.player (id uuid DEFAULT gen_random_uuid() NOT NULL, "googleId" varchar NOT NULL, PRIMARY KEY (id));
+DROP TABLE IF EXISTS public.player CASCADE;
+
+CREATE TABLE public.player (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    "googleId" varchar NOT NULL,
+    PRIMARY KEY (id)
+);
